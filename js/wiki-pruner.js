8672913f41839879296824a74e1d5cdb5b906278
Tim Pettersen <tim@atlassian.com>
2014-03-26 17:22:27 -0700

    style and physics tweaks

--- js/wiki-pruner.js
+++ js/wiki-pruner.js
@@ -26,7 +26,7 @@ getHostJs(function() {
 
   var options = {
     width: $window.width + 'px',
-    height: $window.height + 'px',
+    height: '1200px',
     physics: {
         barnesHut: {
             enabled: true,
