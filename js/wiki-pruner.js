95030dc6a55f16676fbc5b2651ee397244bde69a
Tim Pettersen <tim@atlassian.com>
2014-03-26 17:08:40 -0700

    style and physics tweaks

--- js/wiki-pruner.js
+++ js/wiki-pruner.js
@@ -76,7 +76,7 @@ getHostJs(function() {
           background: background.toHexString(), 
           border: border.toHexString()
         },
-        fontColor: ageRatio > 35 ? "#000000" : "#ffffff"
+        fontColor: ageRatio > 40 ? "#000000" : "#ffffff"
     }
   }
 
